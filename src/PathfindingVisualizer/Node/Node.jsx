42f1656088784ae825f442b9cf49e70e72ecbb84
Joseph Johnson <jwjbadger@gmail.com>
2020-06-17 17:05:56 -0500

    Add basic grid (15 rows 50 columns)

--- src/PathfindingVisualizer/Node/Node.jsx
+++ src/PathfindingVisualizer/Node/Node.jsx
@@ -3,11 +3,8 @@ import React, {Component} from 'react';
 import './Node.css';
 
 export default class Node extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {}
-  }
   render() {
-    return <div className="node"></div>
+    const {row, col, key} = this.props
+    return <div id = {`node-${row}-${col}`} className="node"></div>
   }
 }
--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -7,11 +7,48 @@ import './PathfindingVisualizer.css';
 export default class PathfindingVisualizer extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            grid: []
+        };
     }
+
+    componentDidMount() {
+        const grid = getInitialGrid();
+        this.setState({grid});
+    }
+
     render() {
+        const {grid} = this.state;
+
         return(
-            <Node></Node>
+            <div className="grid">
+                {grid.map((row, rowIdx) => {
+                    return (
+                        <div key={rowIdx}>
+                            {row.map((node, nodeIdx) => {
+                                const {row, col} = node;
+                                return (<Node
+                                    row = {row}
+                                    col = {col}
+                                    key = {nodeIdx}
+                                ></Node>);
+                            })}
+                        </div>
+                    );
+                })}
+            </div>
         );
     }
 }
+
+const getInitialGrid = () => {
+    const grid = [];
+    for (let row = 0; row < 15; row++) {
+        const currentRow = [];
+        for (let col = 0; col < 50; col++) {
+            currentRow.push({row, col});
+        }
+        grid.push(currentRow)
+    }
+    return grid;
+};
